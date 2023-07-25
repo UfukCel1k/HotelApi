f0d7bca16edc40837a2d525e44e9ed8d8929306b
Ufuk Çelik <ufukcelik.dev@gmail.com>
2023-07-26 00:52:41 +0300

    HotelApi

---
+++ Frontend/HotelProject.WebUI/Controllers/RoomController.cs
@@ -0,0 +1,16 @@
+﻿using HotelProject.WebUI.Dtos.RoomDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RoomController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+
+        }
+    }
+}
--- Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.RoomDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _OurRoomsPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _OurRoomsPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //Bir tane client oluştur.
+            var client = _httpClientFactory.CreateClient();
+            //http://localhost:5030/api/Room adresten istekte bulunuyoruz.
+            var responseMessage = await client.GetAsync("http://localhost:5030/api/Room");
+            //Eğer istek yolladığımız adresten başarılı bir durum kodu dönerse gelen veriyi Jsondata ya atıyoruz. 
+            //jsondaki veriyi deserilaze ederek listeledik.
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDto>>(jsonData);
+                return View(values);
+
+            }
             return View();
         }
     }
