cd7d40daf409e607f56d7e1bd8a6ca49def00f0c
Ufuk Çelik <ufukcelik.dev@gmail.com>
2023-10-23 21:58:12 +0300

    HotelApi

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_ServicePartial.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.RoomDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _ServicePartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServicePartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //Bir tane client oluştur.
+            var client = _httpClientFactory.CreateClient();
+            //http://localhost:5030/api/Room adresten istekte bulunuyoruz.
+            var responseMessage = await client.GetAsync("http://localhost:5030/api/Service");
+            //Eğer istek yolladığımız adresten başarılı bir durum kodu dönerse gelen veriyi Jsondata ya atıyoruz. 
+            //jsondaki veriyi deserilaze ederek listeledik.
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+
+            }
             return View();
         }
     }
